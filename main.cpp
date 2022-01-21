9a9814d01440273522850bd61d3db310c72257c2
bvchau <bchau010@ucr.edu>
2022-01-20 16:54:50 -0800

    Edited c echoa and cmakelists

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-	std::string name;
-	std::cout << "What is your name?" << std::endl;
-	std::cin >> name;
-	std::cout << "Hello " << name << "!" << std::endl;
-
+int main(){
+	std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 	return 0;
 }
 
